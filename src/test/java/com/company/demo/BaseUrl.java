be02b9ca9e1ddc1bb5107b6652ae7d60d4b2c09a
Wizard2069 <wizard201366@gmail.com>
2020-09-29 20:19:26 +0700

    Fix host

--- src/test/java/com/company/demo/BaseUrl.java
+++ src/test/java/com/company/demo/BaseUrl.java
@@ -2,12 +2,12 @@ package com.company.demo;
 
 public class BaseUrl {
     
-    private static String host = "app.gopanda.asia"; // app.gopanda.asia
+    private static String host = "localhost"; // app.gopanda.asia
     private static int port = 8080;
     private static String apiPath = "api"; // api
     
     public static String getBaseUrl(String path) {
-        return String.format("https://%s/%s%s", host, apiPath, path);
+        return String.format("http://%s:%s/%s%s", host, port, apiPath, path);
     }
     
 }
